4c4e5de5563b052b38e1ec41482e45adb131a5cf
Pamela Ubuntu <kaue.sedrez@gmail.com>
2024-06-03 15:51:17 -0300

    1.18.15

--- wbr.js
+++ wbr.js
@@ -239,7 +239,6 @@ class WBR {
           out: "./release",
           sass: [
             {
-              firstFile: null,
               entryFolder: "./sass",
               outFolder: "./release",
             },
@@ -300,7 +299,7 @@ class WBR {
     if (global.args.watch === false) return;
 
     new Drawer().drawBlankLine();
-    const refresh = (name) => {
+    const refresh = name => {
       new Drawer().drawChange(name);
       global.idList = [];
       global.promisesConstructos = [];
@@ -357,7 +356,7 @@ class WBR {
 
     if (global.config.sass) {
       let folders = [];
-      global.config.sass.forEach((item) => {
+      global.config.sass.forEach(item => {
         folders.push(item.entryFolder);
       });
       try {
@@ -475,28 +474,36 @@ class WBR {
           },
         },
         (err, stats) => {
+          if (err) {
+            new Drawer().drawError(err.message);
+            new Drawer().drawWarning(
+              "Winnetou is exiting with compilation error. Please, fix your code and run wbr again."
+            );
+            process.exit();
+          }
           // console.log({ err, stats });
-          const info = stats.toJson();
-          stats.compilation.errors.forEach((err) => {
+          const info = stats?.toJson();
+          stats?.compilation.errors.forEach(err => {
             console.log(err.message);
           });
-          if (stats.hasErrors()) {
-            console.error(info.errors);
+          if (stats?.hasErrors()) {
+            console.error(info?.errors);
           }
 
-          if (stats.hasWarnings()) {
-            console.warn(info.warnings);
+          if (stats?.hasWarnings()) {
+            console.warn(info?.warnings);
           }
 
           if (global.compiledFiles >= global.totalFiles) {
             let d = new Drawer();
 
             d.drawBlankLine();
-            d.drawAdd(
-              `Bundle rebuild successful in ` +
-                (stats.endTime - stats.startTime) +
-                "ms"
-            );
+            if (stats)
+              d.drawAdd(
+                `Bundle rebuild successful in ` +
+                  (stats.endTime - stats.startTime) +
+                  "ms"
+              );
             d.drawLine();
           }
           global.compiledFiles++;
@@ -505,10 +512,11 @@ class WBR {
 
       if (global.args.watch) {
         compiler.watch({}, (e, s) => {
-          if (e.name !== "ConcurrentCompilationError") {
-            console.log(e);
-            new Drawer().drawError(e.message);
-          }
+          if (e)
+            if (e?.name !== "ConcurrentCompilationError") {
+              console.log(e);
+              new Drawer().drawError(e.message);
+            }
           if (s && s.compilation.errors.length > 0) {
             new Drawer().drawError(s.compilation.errors.toString());
           }
@@ -521,10 +529,11 @@ class WBR {
         });
       } else {
         compiler.run((e, s) => {
-          if (e.name !== "ConcurrentCompilationError") {
-            console.log(e);
-            new Drawer().drawError(e.message);
-          }
+          if (e)
+            if (e.name !== "ConcurrentCompilationError") {
+              console.log(e);
+              new Drawer().drawError(e.message);
+            }
           if (s && s.compilation.errors.length > 0) {
             new Drawer().drawError(s.compilation.errors.toString());
           }
@@ -562,11 +571,11 @@ class Icons {
         }
       }
 
-      Promise.all(global.promisesIcons).then(async (res) => {
+      Promise.all(global.promisesIcons).then(async res => {
         let splitter = new Array();
         splitter["icons"] = [];
 
-        res.forEach((item) => {
+        res.forEach(item => {
           if (typeof item == "object") {
             let s = item.iconPath.split("/");
             if (s.length > 2) {
@@ -580,7 +589,7 @@ class Icons {
 
         let finalPromise = [];
 
-        Object.keys(splitter).forEach((key) => {
+        Object.keys(splitter).forEach(key => {
           finalPromise.push(
             fs.outputFile(
               path.join(
@@ -592,7 +601,7 @@ class Icons {
           );
         });
 
-        Promise.all(finalPromise).then((res) => {
+        Promise.all(finalPromise).then(res => {
           return resolve(true);
         });
       });
@@ -608,7 +617,7 @@ class Icons {
 
         let id = iconPath.match(regPath);
 
-        id = id.filter((x) => x != "svg");
+        id = id.filter(x => x != "svg");
 
         id = id.join("_");
 
@@ -678,8 +687,8 @@ class Constructos {
             return resolve(true);
           }
           files2 = files2
-            .filter((x) => x.includes("win-"))
-            .filter((x) => x.includes(".htm") || x.includes(".html"));
+            .filter(x => x.includes("win-"))
+            .filter(x => x.includes(".htm") || x.includes(".html"));
 
           if (files2.length > 0) {
             try {
@@ -718,14 +727,14 @@ class Constructos {
   async transpileConstructo(filePath) {
     return new Promise((resolve, reject) => {
       try {
-        new Files().getFileFromCacheAsync(filePath).then((data) => {
+        new Files().getFileFromCacheAsync(filePath).then(data => {
           // transforma o html em método
           let dom = htmlParser.parse(data);
           let components = dom.querySelectorAll("winnetou");
           let finalReturn = "";
           let constructos = [];
 
-          Array.from(components).forEach((component) => {
+          Array.from(components).forEach(component => {
             let descri = component.getAttribute("description");
             let constructo = component.innerHTML;
             let jsdoc = "\n\n// ========================================";
@@ -750,7 +759,7 @@ class Constructos {
 
             let pureId = id + "-win-${identifier}";
 
-            let verify = global.idList.filter((data) => data.id === id);
+            let verify = global.idList.filter(data => data.id === id);
 
             //duplicated constructo
             if (verify.length > 0) {
@@ -773,12 +782,12 @@ class Constructos {
 
             let ids = "ids:{";
 
-            matchIds = matchIds.map((item) =>
+            matchIds = matchIds.map(item =>
               item.replace("[[", "").replace("]]", "")
             );
-            matchIds = matchIds.map((item) => item + "-win-${identifier}");
+            matchIds = matchIds.map(item => item + "-win-${identifier}");
 
-            matchIds.forEach((item) => {
+            matchIds.forEach(item => {
               let nome = item.split("-win-")[0];
               ids += nome + ":`" + item + "`,";
             });
@@ -799,7 +808,7 @@ class Constructos {
             let matches = constructo.match(regex);
 
             if (matches) {
-              matches.forEach((match) => {
+              matches.forEach(match => {
                 let el = match.replace("{{", "");
                 el = el.replace("}}", "");
 
@@ -914,7 +923,7 @@ class Constructos {
 
   async execPromisesConstructos() {
     return new Promise((resolve, reject) => {
-      Promise.all(global.promisesConstructos).then(async (data) => {
+      Promise.all(global.promisesConstructos).then(async data => {
         /**
          * data[0].method
          * data[0].constructosList
@@ -969,8 +978,8 @@ class Constructos {
       recursive("./node_modules", async (err2, files2) => {
         if (err2) files2 = [];
         files2 = files2
-          .filter((x) => x.includes("win-"))
-          .filter((x) => x.includes(".htm") || x.includes(".html"));
+          .filter(x => x.includes("win-"))
+          .filter(x => x.includes(".htm") || x.includes(".html"));
 
         if (files2.length > 0) {
           try {
@@ -980,17 +989,17 @@ class Constructos {
           }
         }
 
-        files = files.map((x) => path.parse(x).name);
+        files = files.map(x => path.parse(x).name);
 
         recursive(constructosOut, async (err3, jsFiles) => {
-          jsFiles = jsFiles.map((x) => path.parse(x).name);
+          jsFiles = jsFiles.map(x => path.parse(x).name);
 
           let diff = this.diffArray(files, jsFiles);
 
-          diff = diff.map((x) => path.join("./", constructosOut, x + ".js"));
+          diff = diff.map(x => path.join("./", constructosOut, x + ".js"));
 
-          diff.forEach((item) => {
-            fs.unlink(item, (e) => {});
+          diff.forEach(item => {
+            fs.unlink(item, e => {});
           });
         });
       });
@@ -1000,7 +1009,7 @@ class Constructos {
   diffArray(arr1, arr2) {
     return arr1
       .concat(arr2)
-      .filter((val) => !(arr1.includes(val) && arr2.includes(val)));
+      .filter(val => !(arr1.includes(val) && arr2.includes(val)));
   }
 }
 
@@ -1028,7 +1037,7 @@ class Translator {
           }
           let trad = xml.parse(data)[0].childNodes;
 
-          trad.forEach((item) => {
+          trad.forEach(item => {
             if (item.tagName && item.childNodes[0]?.text) {
               strings += `
                   /** @property ${item.childNodes[0].text.trim()} */           
@@ -1056,7 +1065,7 @@ class Translator {
               indent_size: 2,
               space_in_empty_paren: true,
             }),
-            (err) => {
+            err => {
               new Drawer().drawAdd("Strings");
               return resolve(true);
             }
@@ -1088,7 +1097,7 @@ class Translator {
             return resolve(true);
           }
 
-          Object.keys(file).map((key) => {
+          Object.keys(file).map(key => {
             let value = file[key];
             strings += `
                 /** @property ${value} */           
@@ -1112,7 +1121,7 @@ class Translator {
               indent_size: 2,
               space_in_empty_paren: true,
             }),
-            (err) => {
+            err => {
               new Drawer().drawAdd("Strings");
               return resolve(true);
             }
@@ -1128,17 +1137,17 @@ class Sass {
     return new Promise((resolve, rejects) => {
       let promises = [];
       let arr = global.config.sass;
-      arr.forEach((item) => {
+      arr?.forEach(item => {
         promises.push(
           this.transpileFolder(item.entryFolder, item.outFolder, item.firstFile)
         );
       });
 
       Promise.all(promises)
-        .then((res) => {
+        .then(res => {
           return resolve(true);
         })
-        .catch((e) => {
+        .catch(e => {
           new Drawer().drawError(e);
           return resolve(true);
         });
@@ -1188,10 +1197,10 @@ class Sass {
     return new Promise(async (resolve, reject) => {
       sass
         .compileAsync(file)
-        .then((res) => {
+        .then(res => {
           return resolve(res.css.toString());
         })
-        .catch((e) => {
+        .catch(e => {
           new Drawer().drawWarning(e.message);
           return reject(e);
         });
@@ -1201,7 +1210,7 @@ class Sass {
   async execSassPromises(out, promisesArray) {
     return new Promise((resolve, reject) => {
       Promise.all(promisesArray)
-        .then((data) => {
+        .then(data => {
           // data has all css files
           data.push(
             `    
@@ -1227,7 +1236,7 @@ class Sass {
             }
           );
         })
-        .catch((e) => {
+        .catch(e => {
           new Err().e004();
           return resolve(true);
         });
@@ -1324,7 +1333,7 @@ class Drawer {
 
   drawTextBlock = (text, params) => {
     let arr = text.match(/.{1,74}/g);
-    arr.forEach((item) => {
+    arr.forEach(item => {
       this.drawText(item, params);
     });
   };
@@ -1341,7 +1350,7 @@ class Drawer {
    * Draw error
    * @param {string} text error string
    */
-  drawError = (text) => {
+  drawError = text => {
     global.errorsCount++;
     this.drawLine();
     this.drawBlankLine();
@@ -1373,7 +1382,7 @@ class Drawer {
     this.drawLine();
   };
 
-  drawWarning = (text) => {
+  drawWarning = text => {
     global.warningCount++;
     this.drawLine();
     this.drawBlankLine();
@@ -1423,29 +1432,29 @@ class Drawer {
    * Draw add
    * @param {string} text add string
    */
-  drawAdd = (text) => {
+  drawAdd = text => {
     this.drawText(text, { type: "add", color: "green" });
     this.drawBlankLine();
   };
 
-  drawChange = (text) => {
+  drawChange = text => {
     this.drawBlankLine();
     this.drawText(text, { type: "change", color: "green" });
     this.drawBlankLine();
   };
 
-  drawAddError = (text) => {
+  drawAddError = text => {
     global.errorsCount++;
 
     this.drawText(text, { type: "addError", color: "cyan" });
     this.drawBlankLine();
   };
 
-  drawHtmlMin = (text) => {
+  drawHtmlMin = text => {
     console.log("> [html minified] " + text);
   };
 
-  drawEnd = (text) => {
+  drawEnd = text => {
     console.log("> [Bundle Release Finished] " + text);
   };
 
