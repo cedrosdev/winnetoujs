0c3b4eb2ed20c525a11d131c6cd73cb9aec96312
Pamela <pamela@cedros.dev>
2025-09-26 14:23:16 -0300

    3.0.0-beta.45

--- dist/modules/ssr.d.ts
+++ dist/modules/ssr.d.ts
@@ -1,5 +1,22 @@
+/**
+ * Escape HTML special characters in a string.
+ * @param {string} v - The input string.
+ * @returns {string} - The escaped string.
+ */
 export declare const escapeHTML: (v: string) => string;
+/**
+ * Simple SSR template literal tag function
+ * @param {any[]} parts - Parts to join
+ * @returns {string}
+ */
 export declare const ssr: (...parts: any[]) => string;
+/**
+ * Load a partial file, caching its content for future use.
+ * @param {string} fileName - Path to the partial file
+ * @param {Object} [options]
+ * @param {boolean} [options.verbose] - Whether to log loading info
+ * @returns {string}
+ */
 export declare function loadPartial(fileName: string, options?: {
     verbose?: boolean;
 }): string;
