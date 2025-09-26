66bef39f791798a782118ebdfa4334a9121d1038
Pamela <pamela@cedros.dev>
2025-09-26 11:40:04 -0300

    3.0.0-beta.44

--- dist/modules/ssr.d.ts
+++ dist/modules/ssr.d.ts
@@ -1,4 +1,6 @@
 export declare const escapeHTML: (v: string) => string;
 export declare const ssr: (...parts: any[]) => string;
-export declare function loadPartial(fileName: string): string;
+export declare function loadPartial(fileName: string, options?: {
+    verbose?: boolean;
+}): string;
 //# sourceMappingURL=ssr.d.ts.map
--- dist/modules/ssr.js
+++ dist/modules/ssr.js
@@ -9,9 +9,16 @@ const ESC = {
 const escapeHTML = (v) => v.replace(/[&<>"']/g, (ch) => ESC[ch]);
 const ssr = (...parts) => parts.flat(Infinity).join("");
 const partials = /* @__PURE__ */ new Map();
-function loadPartial(fileName) {
+function loadPartial(fileName, options) {
   if (!partials.has(fileName)) {
+    if (options?.verbose) {
+      console.log(`Loading partial from disk: ${fileName}`);
+    }
     partials.set(fileName, fs.readFileSync(fileName, "utf8"));
+  } else {
+    if (options?.verbose) {
+      console.log(`Using cached partial: ${fileName}`);
+    }
   }
   return partials.get(fileName);
 }
